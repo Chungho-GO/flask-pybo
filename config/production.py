a746f0d243fbcff996187d79f496d67786257358
고충호 <rhcndgh20@naver.com>
2023-05-07 18:17:05 +0900

    Log 등록

--- config/production.py
+++ config/production.py
@@ -1,33 +1,30 @@
 from config.default import *
 from logging.config import dictConfig
 
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR,"pybo.db")}'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = b'\x08a\t\x14\x17\x04\xf1\x90\xaf\xb7@\xdfGizw'
+
 dictConfig({
     'version': 1,
-    'formatters':{
-        'defalut' : {
+    'formatters': {
+        'default': {
             'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
         }
-    
     },
-
-    'handlers':{
-        'file' : {
-            'level':'INFO',
-            'class' : 'logging.handlers.RotatingFileHandler',
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
-            'maxBytes': 1024 * 1024 * 5 ,
-            'backupCount' : 5,
-            'formatter' : 'default',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
         },
     },
-    'root' : {
-        'level':'INFO',
+    'root': {
+        'level': 'INFO',
         'handlers': ['file']
     }
 })
 
-
-
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR,"pybo.db")}'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'\x08a\t\x14\x17\x04\xf1\x90\xaf\xb7@\xdfGizw'
