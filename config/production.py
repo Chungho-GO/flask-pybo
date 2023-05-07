c2312b47f31fe83df9b12ab9bb1d31e1d045a2e0
고충호 <rhcndgh20@naver.com>
2023-05-07 17:34:04 +0900

    Log 등록

--- config/production.py
+++ config/production.py
@@ -1,4 +1,32 @@
 from config.default import *
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters':{
+        'defalut' : {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    
+    },
+
+    'handlers':{
+        'file' : {
+            'level':'INFO',
+            'class' : 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5 ,
+            'backupCount' : 5,
+            'formatter' : 'default',
+        },
+    },
+    'root' : {
+        'level':'INFO',
+        'handlers': ['file']
+    }
+})
+
+
 
 SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR,"pybo.db")}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
